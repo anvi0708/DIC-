432fe4dcd38366cdb264dc6ae6c71a1a677f6243
anvi0708 <119619109+anvi0708@users.noreply.github.com>
2022-12-01 14:49:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import  LabelEncoder
 import xgboost as xgb
 import numpy as np
-st.header("Fish Weight Prediction App")
+st.header("Claim Prediction app")
 st.text_input("Enter your Name: ", key="name")
 data = pd.read_csv("dic.csv")
 #load label encoder
@@ -18,18 +18,18 @@ best_xgboost_model.load_model("best_model.json")
 if st.checkbox('Show Training Dataframe'):
     data
 
-st.subheader("Please select relevant features of your fish!")
+st.subheader("Please select relevant features of your Product")
 left_column, right_column = st.columns(2)
 with left_column:
     inp_species = st.radio(
-        'Name of the fish:',
-        np.unique(data['Species']))
+        'Name of the Product',
+        np.unique(data['Prod_Name']))
 
 
 input_Agency = st.slider('enter agency name', 0.0, max(data["Agency"]), 1.0)
 input_Agency_Type = st.slider('Diagonal length(cm)', 0.0, max(data["Agency_Type"]), 1.0)
 input_Dist_Channel = st.slider('Cross length(cm)', 0.0, max(data["Dist_Channel"]), 1.0)
-input_Prod_Name = st.slider('Height(cm)', 0.0, max(data["Prod_Name"]), 1.0)
+//input_Prod_Name = st.slider('Height(cm)', 0.0, max(data["Prod_Name"]), 1.0)
 input_Duration = st.slider('Diagonal width(cm)', 0.0, max(data["Duration"]), 1.0)
 input_Destination = st.slider('Diagonal width(cm)', 0.0, max(data["Destination"]), 1.0)
 input_Net_Sales = st.slider('Diagonal width(cm)', 0.0, max(data["Net_Sales"]), 1.0)
@@ -37,12 +37,12 @@ input_Commission = st.slider('Diagonal width(cm)', 0.0, max(data["Commission"]),
 input_Age = st.slider('Diagonal width(cm)', 0.0, max(data["Age"]), 1.0)
 
 if st.button('Make Prediction'):
-    input_species = encoder.transform(np.expand_dims(inp_species, -1))
+    input_Prod_Name = encoder.transform(np.expand_dims(inp_Prod_Name, -1))
     inputs = np.expand_dims(
-        [int(input_species), input_Length1, input_Length2, input_Length3, input_Height, input_Width], 0)
+        [int(input_Prod_Name), input_Agency, input_Agency_Type, input_Dist_Channel, input_Duration, input_Destination, input_Net_Sales, input_Commission, input_Age], 0)
     prediction = best_xgboost_model.predict(inputs)
-    print("final pred", np.squeeze(prediction, -1))
-    st.write(f"Your fish weight is: {np.squeeze(prediction, -1):.2f}g")
+    print("final pred", np.squeeze(claim, -1))
+    st.write(f"Your fish weight is: {np.squeeze(claim, -1):.2f}g")
 
     st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
     st.write(f"If you want to see more advanced applications you can follow me on [medium](https://medium.com/@gkeretchashvili)")
