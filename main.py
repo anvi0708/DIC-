4e795753fee147d4838fa7f5cce1ed2458e84a0d
anvi0708 <119619109+anvi0708@users.noreply.github.com>
2022-12-01 15:33:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ with left_column:
         'Name of the Agency',
         np.unique(data['Agency']))   
     
- st.subheader("Please select relevant features of your Agency Type")
+st.subheader("Please select relevant features of your Agency Type")
 left_column, right_column = st.columns(2)
 with left_column:
     inp_Agency_Type = st.radio(
