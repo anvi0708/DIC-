8e0846505deab0371301ca3374ee7e6e74ebab27
anvi0708 <119619109+anvi0708@users.noreply.github.com>
2022-12-01 19:33:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import  LabelEncoder
-import xgboost as xgb
+from xgboost import XGBClassifier
 import numpy as np
 st.header("Claim Prediction app")
 st.text_input("Enter your Name: ", key="name")
@@ -12,8 +12,8 @@ encoder = LabelEncoder()
 encoder.classes_ = np.load('classes.npy',allow_pickle=True)
 
 # load model
-best_xgboost_model = xgb.XGBRegressor()
-best_xgboost_model.load_model("best_model.json")
+#best_xgboost_model = xgb.XGBRegressor()
+#best_xgboost_model.load_model("best_model.json")
 
 if st.checkbox('Show Training Dataframe'):
     data
@@ -62,20 +62,30 @@ input_Net_Sales = st.slider('Enter Net Sales', 0.0, max(data["Net_Sales"]), 100.
 input_Commission = st.slider('Enter Commission Value', 0.0, max(data["Commission"]), 100.0)
 input_Age = st.slider('Enter Age', 0, max(data["Age"]), 100)
 
+x = data.drop(columns='Claim')
+y = data['Claim']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=1)
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train= scaler.fit_transform(x_train)
+x_test= scaler.transform(x_test)
+
+
+xgc = xgb.XGBClassifier()
+
+xgc.fit(x_train,y_train)
+
+#xg_pred = xgc.predict(x_test)
+
 if st.button('Make Prediction'):
-    input_Agency = encoder.transform(np.expand_dims(inp_Agency, -1))
-    input_Agency_Type = encoder.transform(np.expand_dims(inp_Agency_Type, -1))
-    input_Dist_Channel = encoder.transform(np.expand_dims(inp_Dist_Channel, -1))
-    input_Prod_Name = encoder.transform(np.expand_dims(inp_Prod_Name, -1))
-    input_Destination = encoder.transform(np.expand_dims(inp_Destination, -1))
+
     inputs = np.expand_dims(
-        [int(input_Agency),int(input_Agency_Type), int(input_Dist_Channel), int(input_Prod_Name), input_Duration, int(input_Destination), input_Net_Sales, input_Commission, input_Age], 0)
-    prediction = best_xgboost_model.predict(inputs)
-    print("final pred", np.squeeze(claim, -1))
-    st.write(f"Your fish weight is: {np.squeeze(claim, -1):.2f}g")
-
-    st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
-    st.write(f"If you want to see more advanced applications you can follow me on [medium](https://medium.com/@gkeretchashvili)")
-
+        [input_Agency,input_Agency_Type,input_Dist_Channel, input_Prod_Name, input_Duration, input_Destination, input_Net_Sales, input_Commission, input_Age], 0)
+    prediction = xgc.predict(inputs)
+    if prediction==Yes:
+        st.write("Your insurance will be claimed")
+    else: 
+        st.write("Your insurance will not be claimed")
+    
 
 
